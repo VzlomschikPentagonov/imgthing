6dc3dea469ea93b033a671115ee25755392f3ddb
Андреюшин Тимофей Игоревич ИП-415 <5959jhihguoygbio54@gmail.com>
2025-07-15 10:28:11 +0700

    Wrote BMP header writing functions and BMP header struct.

---
+++ func/bmpheader.cpp
@@ -0,0 +1,32 @@
+#include "../include/bmpheader.hpp"
+
+Header set_header(uint16_t width,
+                  uint16_t height){
+    Header header;
+    header.width = width;
+    header.height = width;
+    header.padding = width & 3;
+    header.filesize = width * height * 3 + height * header.padding;
+    return header;
+}
+
+void write_header(Header header,
+                  const int8_t* filename){
+    int8_t* header_buffer = calloc(HEADER_SIZE, SIZEOF_INT8_T));
+    FILE* header_file = fopen("header.bmp", "r+b");
+    FILE* image_file = fopen(filename, "w+b");
+    uint8_t i;
+    fread(&header_buffer, HEADER_SIZE, SIZEOF_INT8_T, header_file);
+    for(i = 0; i < 4; i++){
+		header_buffer[2 + i] = header.filesize + 54 >> I_BYTES;
+		header_buffer[34 + i] = header.filesize >> I_BYTES;
+	}
+	for(i = 0; i < 2; i++){
+        header_buffer[18 + i] = width >> I_BYTES;
+        header_buffer[22 + i] = height >> I_BYTES;
+	}
+    fwrite(&header_buffer, HEADER_SIZE, SIZEOF_INT8_T, image_file);
+    fflush(image_file);
+    fclose(header_file);
+    free(header_buffer);
+}
---
+++ include/bmpheader.hpp
@@ -0,0 +1,20 @@
+#ifndef BMPHEADER_HPP
+#define BMPHEADER_HPP
+
+#include <stdio.h>
+#include <stdlib.h>
+#define HEADER_SIZE 54
+#define SIZEOF_INT8_T 1
+#define I_BYTES (i << 3)
+
+typedef struct Header{
+    uint32_t filesize;
+    uint16_t width;
+    uint16_t height;
+    uint8_t padding;
+}Header;
+
+Header set_header(uint16_t, uint16_t);
+void write_header(Header, const int8_t*);
+
+#endif
