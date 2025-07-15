02626754ace1a2c221094cb61c4c0331dda541ca
Андреюшин Тимофей Игоревич ИП-415 <5959jhihguoygbio54@gmail.com>
2025-07-15 10:55:07 +0700

    Made small changes to the code.

--- include/bmpheader.hpp
+++ include/bmpheader.hpp
@@ -5,13 +5,13 @@
 #include <stdlib.h>
 #define HEADER_SIZE 54
 #define SIZEOF_INT8_T 1
-#define I_BYTES (i << 3)
+#define I_BYTES (i<<3)
 
 typedef struct Header{
-    uint32_t filesize;
-    uint16_t width;
-    uint16_t height;
-    uint8_t padding;
+	uint32_t filesize;
+	uint16_t width;
+	uint16_t height;
+	uint8_t padding;
 }Header;
 
 Header set_header(uint16_t, uint16_t);
