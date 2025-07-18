046a7323e8a039489704a076eae56294ed1f8b8d
Андреюшин Т. И. ИП-415 <5959jhihguoygbio54@gmail.com>
2025-07-19 01:08:08 +0700

    test commit

--- cmt.sh
+++ cmt.sh
@@ -4,18 +4,12 @@ read COMMIT_NAME
 git add .
 git commit -m "$COMMIT_NAME"
 read PUSH_BRANCH
-if[$PUSH_BRANCH != ""]; then
- git push img $PUSH_BRANCH
-fi
-#echo "Push (y/n)"
-#read YN
-#case $YN in
-# y)
-#  echo "Input branch name:"
-#  read BRANCH_NAME
-#  git push img $BRANCH_NAME
-# ;;
-# n)
-# ;;
-#esac
+case $PUSH_BRANCH in
+ )
+  break
+  ;;
+ *)
+  git push img $PUSH_BRANCH
+  ;;
+esac
 clear
