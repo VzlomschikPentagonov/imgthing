bc99e59c858ae10b745d968097092c9eeba715c7
Андреюшин Т. И. ИП-415 <5959jhihguoygbio54@gmail.com>
2025-07-19 00:54:58 +0700

    Reworked commit scripts

--- cmt.sh
+++ cmt.sh
@@ -3,14 +3,19 @@ echo "Input commit name:"
 read COMMIT_NAME
 git add .
 git commit -m "$COMMIT_NAME"
-echo "Push (y/n)"
-read YN
-case $YN in
- y)
-  echo "Input branch name:"
-  read BRANCH_NAME
-  git push img $BRANCH_NAME
- ;;
- n)
- ;;
-esac
+read PUSH_BRANCH
+if[$PUSH_BRANCH != ""]; then
+ git push img $PUSH_BRANCH
+fi
+#echo "Push (y/n)"
+#read YN
+#case $YN in
+# y)
+#  echo "Input branch name:"
+#  read BRANCH_NAME
+#  git push img $BRANCH_NAME
+# ;;
+# n)
+# ;;
+#esac
+clear
